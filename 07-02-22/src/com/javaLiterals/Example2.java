2151dc4a64c4380a667025bf94f62391ad1c10a6
PALLAVI <PALLAVI@LAPTOP-BUP8J5B8>
2022-02-07 11:47:03 +0530

    com.javaLiterals

---
+++ 07-02-22/src/com/javaLiterals/Example2.java
@@ -0,0 +1,48 @@
+package com.javaLiterals;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		
+		//Write a java program to print student information using java literals
+		
+		int studentId;
+		String studentName;
+		int studentAge;
+		float studentMarks;
+		String studentGrade;
+		String studentResult;
+		String studentAddress;
+		
+		studentId = 4483;
+		studentName = "Pallavi";
+		studentAge = 23;
+		studentMarks = 58.45f;
+		studentGrade = "A";
+		studentResult = "Pass";
+		studentAddress = "Pune";
+		
+		System.out.print("Student Id:");
+		System.out.println(studentId);
+		
+		System.out.print("Student Name:");
+		System.out.println(studentName);
+		
+		System.out.print("Student Age:");
+		System.out.println(studentAge);
+		
+		System.out.print("Student Marks:");
+		System.out.println(studentMarks);
+		
+		System.out.print("Student Grade:");
+		System.out.println(studentGrade);
+		
+		System.out.print("Student Result:");
+		System.out.println(studentResult);
+		
+		System.out.print("Student Address:");
+		System.out.println(studentAddress);
+
+	}
+
+}
