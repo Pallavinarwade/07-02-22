76cc938801eccc79c5c464ee4974ed3bed29dd3a
PALLAVI <PALLAVI@LAPTOP-BUP8J5B8>
2022-02-07 13:04:14 +0530

    com.javaDatatype

---
+++ 07-02-22/src/com/javaDatatypes/Example4.java
@@ -0,0 +1,21 @@
+package com.javaDatatypes;
+
+public class Example4 {
+
+	public static void main(String[] args) {
+		
+		//int employeeids = 480,481,482,483,484,485;
+		//System.out.println(employeeids);
+		
+		int employeeids[] = {480,481,482,483,484,485};
+		//System.out.println(employeeids[]);
+		
+		for(int employeeid : employeeids)
+		{
+			System.out.println(employeeid);
+		}
+		
+
+	}
+
+}
