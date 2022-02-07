49a7e5f2a23f8fe49f5c98b29d4f5289e4524746
PALLAVI <PALLAVI@LAPTOP-BUP8J5B8>
2022-02-07 12:53:46 +0530

    com.javaDatatype

---
+++ 07-02-22/src/com/javaDatatype/Example3.java
@@ -0,0 +1,22 @@
+package com.javaDatatype;
+
+public class Example3 {
+
+	public static void main(String[] args) {
+		
+		//int ages = 23,24,25,26,27;
+		//System.out.println(ages);
+		
+		int ages[] = {23,24,25,26,27};
+		
+		//System.out.println(ages[]);
+		
+		for(int age : ages)
+			
+		{
+			System.out.println(age);
+		}
+
+	}
+
+}
